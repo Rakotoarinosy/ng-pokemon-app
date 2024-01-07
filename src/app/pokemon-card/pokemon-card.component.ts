ab746107a61be25f383c09d2d80f27ed4237c226
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-01-07 14:53:56 +0300

    The pipes

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Pokemon } from '../pokemon';
 import { BorderCardDirective } from '../border-card.directive';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 @Component({
   selector: 'app-pokemon-card',
   standalone: true,
-  imports: [BorderCardDirective],
+  imports: [BorderCardDirective, CommonModule, PokemonTypeColorPipe],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css'
 })
