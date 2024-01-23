31e8f8dd5cda192fd93c9bcfa564fa34e7e7626a
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-01-23 22:58:22 +0300

    setup HttpClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    InMemoryDataService,
+    // HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false})
+  ]
 })
 export class AppComponent {
 }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,13 @@
+import { POKEMONS } from './pokemon/mocks/mock-pokemon-list';
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    return {POKEMONS};
+  }
+}
